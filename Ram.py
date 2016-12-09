39eddce3ad377c02facac0c10968134bace2867e
Vault108 <Vault108@users.noreply.github.com>
2016-12-08 17:27:39 -0800

    added ram

---
+++ Ram.py
@@ -0,0 +1,11 @@
+##this script will show your free Ram by default its set to use commands from version 3.2.8
+## if you have a version higher please uncomment the lines at the bottom and remove the lines bellow:
+import os 
+
+os.system('free -m')
+
+# Remove the # on the lines below
+# import os
+# os.system('free -h')
+
+# and thiss
